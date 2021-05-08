d4096958e86ab6d9a56c9019d37971bf3493162c
Оберман Игорь <oberman.ie@gmail.com>
2021-05-09 00:02:30 +0300

    Add buttons

--- menu.h
+++ menu.h
@@ -1,5 +1,6 @@
 #pragma once
 #include"errors.h"
+#include"button.h"
 #include<iostream>
 
 using namespace sf;
@@ -7,14 +8,14 @@ using namespace sf;
 class Menu
 {
     private:
-        Texture string_start_texture,
-                string_settings_texture,
-                string_exit_texture,
-                settings_texture;
+        Button *start,
+               *settings,
+               *exit;
         MapWidget *background,
-                  *settings;
-        bool status;
-        void SettingsWindow(RenderWindow& window, MapWidget* settings);
+                  *settings_map;
+        Color main_color,
+              button_color;
+        void SettingsWindow(RenderWindow& window);
 
     public:
         Menu();
@@ -24,87 +25,70 @@ class Menu
 
 Menu::Menu()
 {
-    if(!string_start_texture.loadFromFile("images/string_start.png"))
-        exit(WRONG_PICTURE);
-    if(!string_settings_texture.loadFromFile("images/string_settings.png"))
-        exit(WRONG_PICTURE);
-    if(!string_exit_texture.loadFromFile("images/string_exit.png"))
-        exit(WRONG_PICTURE);
+    button_color = Color(126, 0, 0);
+    main_color = Color(126, 181, 221);
+    start = new Button("images/string_start.png", 30, 10, button_color);
+    settings = new Button("images/string_settings.png", 30, 70, button_color);
+    exit = new Button("images/string_exit.png", 30, 130, button_color);
     background = new MapWidget(0, 0, 1900, 900, "images/background.png");
-    settings = new MapWidget(0, 0, 1900, 900, "images/settings.png");
-    status = true;
+    settings_map = new MapWidget(0, 0, 1900, 900, "images/settings.png");
 }
 
 void Menu::run(RenderWindow& window)
 {
-    Sprite string_start(string_start_texture),
-           string_settings(string_settings_texture),
-           string_exit(string_exit_texture);
-
-    string_start.setPosition(30, 10);
-	string_settings.setPosition(30, 70);
-	string_exit.setPosition(30, 130);
-	while(window.isOpen() && status)
+	while(window.isOpen())
     {
+        start->SetColor(button_color);
+        settings->SetColor(button_color);
+        exit->SetColor(button_color);
         Event event;
         while(window.pollEvent(event))
             if(event.type == Event::Closed)
                 window.close();
-
-        int num = 0;
-        string_start.setColor(Color(126, 0, 0));
-        string_settings.setColor(Color(126, 0, 0));
-        string_exit.setColor(Color(126, 0, 0));
-        num = 0;
-        window.clear(Color(126, 181, 221));
+        window.clear(main_color);
 
         if(IntRect(30, 10, 180, 40).contains(Mouse::getPosition(window)))
         {
-            string_start.setColor(Color::Blue);
-            num = 1;
+            start->SetColor(Color::Blue);
+            if(Mouse::isButtonPressed(Mouse::Left))
+                break;
         }
         if(IntRect(30, 70, 200, 40).contains(Mouse::getPosition(window)))
         {
-            string_settings.setColor(Color::Blue);
-            num = 2;
+            settings->SetColor(Color::Blue);
+            if(Mouse::isButtonPressed(Mouse::Left))
+                SettingsWindow(window);
         }
         if(IntRect(30, 130, 100, 40).contains(Mouse::getPosition(window)))
         {
-            string_exit.setColor(Color::Blue);
-            num = 3;
-        }
-
-        if(Mouse::isButtonPressed(Mouse::Left))
-        {
-            if(num == 1)
-                status = false;
-            if(num == 2)
-                SettingsWindow(window, settings);
-            if(num == 3)
-            {
+            exit->SetColor(Color::Blue);
+            if(Mouse::isButtonPressed(Mouse::Left))
                 window.close();
-                status = false;
-            }
         }
-        background->Draw(&window);
-        window.draw(string_start);
-        window.draw(string_settings);
-        window.draw(string_exit);
 
+        background->Draw(window);
+        start->Draw(window);
+        settings->Draw(window);
+        exit->Draw(window);
         window.display();
     }
 }
 
 Menu::~Menu()
 {
-    delete background;
+    delete start;
     delete settings;
+    delete exit;
+    delete background;
+    delete settings_map;
 }
 
-void Menu::SettingsWindow(RenderWindow& window, MapWidget* settings)
-{ // add a button
+void Menu::SettingsWindow(RenderWindow& window)
+{
+    Button escape{"images/string_exit.png", 1630, 830, button_color};
     while(window.isOpen())
     {
+        escape.SetColor(button_color);
         Event event;
         while(window.pollEvent(event))
             if(event.type == Event::Closed)
@@ -112,7 +96,14 @@ void Menu::SettingsWindow(RenderWindow& window, MapWidget* settings)
             else if(event.type == Event::KeyPressed &&
                     event.key.code == Keyboard::Escape)
                 return;
-        settings->Draw(&window);
+        if(IntRect(1630, 830, 100, 40).contains(Mouse::getPosition(window)))
+        {
+            escape.SetColor(Color::Blue);
+            if(Mouse::isButtonPressed(Mouse::Left))
+                return;
+        }
+        settings_map->Draw(window);
+        escape.Draw(window);
         window.display();
     }
 }
