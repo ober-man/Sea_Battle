c88ba87a25b35a5900a5291c61b94867ba9e8fd3
Оберман Игорь <oberman.ie@gmail.com>
2021-05-11 01:50:35 +0300

    Update Application.h

--- Application.h
+++ Application.h
@@ -8,12 +8,14 @@ class Application
 {
      private:
         RenderWindow *window;
-        MapWidget *mapwidget;
+        MapWidget *ship_position,
+                  *game;
         Button *random;
         Color button_color,
               main_color;
         Menu menu;
         void StartGame(RenderWindow* window);
+        void DrawBoards(RenderWindow* window, Player& man, Sprite s);
 
      public:
         Application();
@@ -30,15 +32,24 @@ Application::Application()
 	if (!pirateMusic.openFromFile("sounds/pirate.wav"))
 		exit(WRONG_MUSIC);
 	pirateMusic.play();*/
-    mapwidget = new MapWidget(0, 0, 1900, 900, "images/game_begin.png");
+    ship_position = new MapWidget(0, 0, 1900, 900, "images/position.png");
+    game = new MapWidget(0, 0, 1900, 900, "images/table.jpg");
     button_color = Color(126, 0, 0);
     main_color = Color(126, 181, 221);
     random = new Button("images/random.png", 1450, 750, button_color);
 }
 
+Application::~Application()
+{
+    delete window;
+    delete ship_position;
+    delete game;
+    delete random;
+}
+
 void Application::Run()
 {
-    menu.run(*window);
+    menu.run(*window); // return value = game mode
     while(window->isOpen())
     {
         sf::Event event;
@@ -56,17 +67,10 @@ void Application::Run()
                 StartGame(window);
         }
 
-        mapwidget->Draw(*window);
+        ship_position->Draw(*window);
         random->Draw(*window);
         window->display();
     }
-}
-
-Application::~Application()
-{
-    delete window;
-    delete mapwidget;
-    delete random;
 }
 
 void Application::StartGame(RenderWindow* window)
@@ -78,67 +82,110 @@ void Application::StartGame(RenderWindow* window)
 
     Player man;
     man.random_position();
-    int status = 0;
+    int status = 10;
 
     while(window->isOpen())
     {
-        Vector2i pos = Mouse::getPosition(*window);
-        int x_my = (pos.x - x_0) / len;
-        int y_my = (pos.y - y_0) / len;
-        coords XY_my = {x_my, y_my};
-        int x_enemy = (pos.x - x_2) / len;
-        int y_enemy = (pos.y - y_2) / len;
-        coords XY_enemy = {x_enemy, y_enemy};
-
-        Event event;
-        while(window->pollEvent(event))
+        if(status > 0)
         {
-            if(event.type == Event::Closed)
-                window->close();
-            if(event.type == Event::MouseButtonPressed)
-				if(event.key.code == Mouse::Left)
-                {
-                    if(x_my > 0 && y_my > 0 && x_my < LSIZE - 1 && y_my < LSIZE - 1)
-                        // add condition if user press killed cell
-                        status = man.turn(XY_my);
-                        //MyEmptyBoard.Set(x_my, y_my, man.Myboard->Get(x_my, y_my));
-                    if(x_enemy > 0 && y_enemy > 0 && x_enemy < LSIZE - 1 && y_enemy < LSIZE - 1)
-                        status = man.turn(XY_enemy);
-                        //EnemyEmptyBoard.Set(x_enemy, y_enemy, man.Myboard->Get(x_enemy, y_enemy));
-                }
+            if(status == PLAYER_WIN)
+                return; // make a window
+
+            Vector2i pos = Mouse::getPosition(*window);
+            int x_my = (pos.x - x_0) / len;
+            int y_my = (pos.y - y_0) / len;
+            coords XY_my = {x_my, y_my};
+
+            Event event;
+            while(window->pollEvent(event))
+            {
+                if(event.type == Event::Closed)
+                    window->close();
+                if(event.type == Event::MouseButtonPressed)
+                    if(event.key.code == Mouse::Left)
+                        if(x_my > 0 && y_my > 0 && x_my < SIZE && y_my < SIZE)
+                        {
+                            // add condition if user press killed cell
+                            status = man.turn(XY_my);
+                            std::cout << status << std::endl;
+                        }
+            }
+            game->Draw(*window);
+            //window->clear(Color::White);
+            DrawBoards(window, man, s);
+            window->display();
         }
-        window->clear(Color::White);
+        else // status < 0
+        {
+            if(status == PLAYER_LOSE)
+                return;
+            status = man.enemy_turn();
+            std::cout << status << std::endl;
 
-        for(int i = 0; i < LSIZE; i++)
-			for(int j = 0; j < LSIZE; j++)
-			{
-				if(man.Hitboard->Get(i, j) == POISON)
-					s.setTextureRect(IntRect(0, 0, len, len));
-				else if(man.Hitboard->Get(i, j) == HIT_SHIP)
-					s.setTextureRect(IntRect(2 * len, 0, len, len));
-				else if(man.Hitboard->Get(i, j) == HIT_MISS)
-					s.setTextureRect(IntRect(3 * len, 0, len, len));
-				else
-					s.setTextureRect(IntRect(1 * len, 0, len, len));
-				s.setPosition(i*len + x_0, j*len + y_0);
-				window->draw(s);
-			}
-
-        for(int i = 0; i < LSIZE; i++)
-			for(int j = 0; j < LSIZE; j++)
-			{
-			    if(man.HitEnemyboard->Get(i, j) == POISON)
-                    s.setTextureRect(IntRect(0, 0, len, len));
-                else if(man.HitEnemyboard->Get(i, j) == HIT_SHIP)
-                    s.setTextureRect(IntRect(2 * len, 0, len, len));
-                else if(man.HitEnemyboard->Get(i, j) == HIT_MISS)
-                    s.setTextureRect(IntRect(3 * len, 0, len, len));
-                else
-                    s.setTextureRect(IntRect(1 * len, 0, len, len));
-				s.setPosition(i*len + x_2, j*len + y_2);
-				window->draw(s);
-			}
-
-        window->display();
+            Event event;
+            while(window->pollEvent(event))
+            {
+                if(event.type == Event::Closed)
+                    window->close();
+            }
+            game->Draw(*window);
+            //window->clear(Color::White);
+            DrawBoards(window, man, s);
+            window->display();
+        }
     }
+}
+
+void Application::DrawBoards(RenderWindow* window, Player& man, Sprite s)
+{
+    // Upper left corners
+    s.setTextureRect(IntRect(len, 0, len, len));
+    s.setPosition(x_0, y_0);
+    window->draw(s);
+    s.setPosition(x_2, y_2);
+    window->draw(s);
+
+    for(int i = 1; i < SIZE; i++)
+    {
+        s.setTextureRect(IntRect((i + 3) * len, 0, len, len));
+        s.setPosition(i*len + x_0, y_0);
+        window->draw(s);
+        s.setPosition(i*len + x_2, y_2);
+        window->draw(s);
+        s.setPosition(x_0, i*len + y_0);
+        window->draw(s);
+        s.setPosition(x_2, i*len + y_2);
+        window->draw(s);
+    }
+    for(int i = 1; i < LSIZE; i++)
+        for(int j = 1; j < LSIZE; j++)
+        {
+            if(man.HitEnemyboard->Get(i, j) == POISON)
+                continue;
+                //s.setTextureRect(IntRect(0, 0, len, len));
+            else if(man.HitEnemyboard->Get(i, j) == HIT_SHIP)
+                s.setTextureRect(IntRect(2 * len, 0, len, len));
+            else if(man.HitEnemyboard->Get(i, j) == HIT_MISS)
+                s.setTextureRect(IntRect(3 * len, 0, len, len));
+            else
+                s.setTextureRect(IntRect(1 * len, 0, len, len));
+            s.setPosition(i*len + x_2, j*len + y_2);
+            window->draw(s);
+        }
+
+    for(int i = 1; i < LSIZE; i++)
+        for(int j = 1; j < LSIZE; j++)
+        {
+            if(man.Hitboard->Get(i, j) == POISON)
+                continue;
+                //s.setTextureRect(IntRect(0, 0, len, len));
+            else if(man.Hitboard->Get(i, j) == HIT_SHIP)
+                s.setTextureRect(IntRect(2 * len, 0, len, len));
+            else if(man.Hitboard->Get(i, j) == HIT_MISS)
+                s.setTextureRect(IntRect(3 * len, 0, len, len));
+            else
+                s.setTextureRect(IntRect(1 * len, 0, len, len));
+            s.setPosition(i*len + x_0, j*len + y_0);
+            window->draw(s);
+        }
 }
