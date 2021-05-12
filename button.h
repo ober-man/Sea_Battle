a99735214ccf6f7b222304032ef06751a02ea510
Оберман Игорь <oberman.ie@gmail.com>
2021-05-13 01:10:46 +0300

    Update button.h

--- button.h
+++ button.h
@@ -12,22 +12,3 @@ class Button
         void SetColor(const sf::Color& c);
         void Draw(sf::RenderWindow& window);
 };
-
-Button::Button(const char* name, int x, int y, const sf::Color& color) : x_pos(x), y_pos(y)
-{
-    if(!texture.loadFromFile(name))
-        exit(WRONG_PICTURE);
-    spr.setTexture(texture);
-    spr.setPosition(x_pos, y_pos);
-    spr.setColor(color);
-}
-
-void Button::SetColor(const sf::Color& c)
-{
-    spr.setColor(c);
-}
-
-void Button::Draw(sf::RenderWindow& window)
-{
-    window.draw(spr);
-}
