e505ff470911cfcfea9c5581e3e7622171ec4d03
Оберман Игорь <oberman.ie@gmail.com>
2021-05-13 01:26:29 +0300

    Update map.h

--- map.h
+++ map.h
@@ -14,19 +14,3 @@ class MapWidget
         MapWidget(int x_, int y_, int w, int h, const char* str);
         void Draw(sf::RenderWindow& window);
 };
-
-MapWidget::MapWidget(int x_, int y_, int w, int h, const char* str) : x(x_), y(y_), width(w), height(h)
-{
-    if(!texture.loadFromFile(str))
-        exit(WRONG_PICTURE);
-    spr.setTexture(texture);
-}
-
-void MapWidget::Draw(sf::RenderWindow& window)
-{
-    spr.setPosition(x, y);
-    float WidgetWidth = (float) width / (float) texture.getSize().x;
-    float WidgetHeight = (float) height / (float) texture.getSize().y;
-    spr.setScale(WidgetWidth, WidgetHeight);
-    window.draw(spr);
-}
